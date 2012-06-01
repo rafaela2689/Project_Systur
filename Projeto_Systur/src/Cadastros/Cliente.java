8b24098537dc82958fbd1f70bd3c3d7725d57da3
Rafaela Cavalcante <cavalcante_rafa@hotmail.com>
2012-05-31 22:06:52 -0400

    Criaçao da classe cliente

---
+++ Projeto_Systur/src/Cadastros/Cliente.java
@@ -0,0 +1,46 @@
+package Cadastros;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAFAELA
+ */
+public class Cliente {
+    
+    private String nome;
+    private String CPF;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    
+}
