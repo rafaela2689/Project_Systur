46073c1cde9d16f725eb5d87d6bafe281a69a4a7
Adilio Figueiredo <adiliosouza@me.com>
2012-06-03 17:52:06 -0400

    Criando os atributos e metodos da classe veiculo.

---
+++ Projeto_Systur/src/Negocio/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author adiliosouza
+ */
+public class NewClass {
+    //private
+    
+}
