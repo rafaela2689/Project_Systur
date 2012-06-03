edf2fbe31345dc925a2cdb2c6dd29ce6917780ed
Adilio Figueiredo <adiliosouza@me.com>
2012-06-03 15:15:16 -0400

    Criando os atributos e metodos da classe funcionário.

--- Projeto_Systur/src/Negocio/Funcionario.java
+++ Projeto_Systur/src/Negocio/Funcionario.java
@@ -9,5 +9,23 @@ package Negocio;
  * @author RAFAELA
  */
 public class Funcionario {
+    private String Nome;
+    private String RG;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
     
 }
